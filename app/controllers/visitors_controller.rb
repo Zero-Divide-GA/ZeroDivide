8de7892924ff8a0aabda80d8d051238fc3199c55
vikashiscool <vikash_parekh@yahoo.com>
2015-07-17 13:12:49 -0700

    Edit visitors controller actions

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,8 +6,19 @@ class VisitorsController < ApplicationController
   end
 
   def new
+   @visitor = Visitor.new
+   render :new 
   end
 
   def create
+    @visitor = Visitor.create(visitor_params)
+    redirect_to visitors_index_path
   end
+
+private
+
+def visitor_params
+  params.require(:visitor).permit(:first_name, :last_name, :phone, :email, :zip, :language)
+end
+
 end
