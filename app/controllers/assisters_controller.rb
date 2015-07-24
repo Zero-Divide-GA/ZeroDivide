28325fca7eb78f3904ca7f44c8dfaa28d4b21dc1
vikashiscool <vikash_parekh@yahoo.com>
2015-07-24 14:34:11 -0700

    Update assisters controller

--- app/controllers/assisters_controller.rb
+++ app/controllers/assisters_controller.rb
@@ -2,15 +2,33 @@ class AssistersController < ApplicationController
   def index
   end
 
-  def new
-  end
-
-  def new_two
-  end
-
   def show
   end
 
-  def create
+  def new
+   @assister = Assister.new
+   render :new 
   end
+
+  def new_two
+   @assister = Assister.new
+   render :new 
+  end
+
+  def create
+    @assister = Assister.create(visitor_params)
+    puts "Saving visitor"
+    if @assister.save
+      # flash[:success] = "Thank you. You information has been recorded and is being reviewed by our team of professionals. An insurance expert will be contacting you shortly."
+      redirect_to visitors_index_path
+    else
+      render 'new'
+    end
+  end
+
+def assister_params
+  params.require(:assister).permit(:firstname, :lastname, :phone, :email, :zip, :language, :addlanguage)
+end
+
+
 end
